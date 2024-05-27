dccac82a1b439fd67be5e10fcfd19f054e531874
AliyanKhan22 <alyanwasim81@gmail.com>
2024-05-27 13:17:58 -0700

    Zoomer

---
+++ app.js
@@ -0,0 +1,39 @@
+let zoom = document.getElementById('Zoom');
+zoom.addEventListener('mouseover',()=>{
+    zoom.style.transition = '0.s'
+    zoom.style.transform ='scale(1.3)';
+})
+zoom.addEventListener('mouseout',()=>{
+    zoom.style.transition =' 0.3s'
+    zoom.style.transform = 'scale(1)';
+})
+
+let zoom2 = document.getElementById('Zoom2');
+zoom2.addEventListener('mouseover',()=>{
+    zoom2.style.transition = '0.s'
+    zoom2.style.transform ='scale(1.3)';
+})
+zoom2.addEventListener('mouseout',()=>{
+    zoom2.style.transition =' 0.3s'
+    zoom2.style.transform = 'scale(1)';
+})
+
+let zoom3 = document.getElementById('Zoom3');
+zoom3.addEventListener('mouseover',()=>{
+    zoom3.style.transition = '0.s'
+    zoom3.style.transform ='scale(1.3)';
+})
+zoom3.addEventListener('mouseout',()=>{
+    zoom3.style.transition =' 0.3s'
+    zoom3.style.transform = 'scale(1)';
+})
+
+let Zoom4 = document.getElementById('Zoom4');
+Zoom4.addEventListener('mouseover',()=>{
+    Zoom4.style.transition = '0.s'
+    Zoom4.style.transform ='scale(1.3)';
+})
+Zoom4.addEventListener('mouseout',()=>{
+    Zoom4.style.transition =' 0.3s'
+    Zoom4.style.transform = 'scale(1)';
+})
